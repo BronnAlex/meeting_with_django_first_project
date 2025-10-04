dd3032f9356714183b51ad0a31b8bb1a61ea6d34
Aleksey <lesha.bronnikov.99@mail.ru>
2025-10-04 19:42:15 +0300

    создал миграции и отправил их в БД

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from catalog.apps import CatalogConfig
+from django.conf import settings
+from django.conf.urls.static import static
 
-name_space = CatalogConfig.name
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
-        "", include("catalog.urls", namespace=name_space)
+        "", include("catalog.urls", namespace="catalog")
     ),  # Путь к нашему приложению catalog
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, documment_root=settings.MEDIA_ROOT)
