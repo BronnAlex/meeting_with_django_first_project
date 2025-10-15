4d6156bf8ddd6cc09ae869fe80d96a67d777eef8
Aleksey <lesha.bronnikov.99@mail.ru>
2025-10-15 20:16:48 +0300

    Перевел контроллеры из fbv в cbv

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,20 +2,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.urls import path
-from catalog.views import (
-    home,
-    contacts,
-    product_details,
-)
+from catalog.views import CatalogListView, CatalogDetailView, CatalogTemplateView
 
 app_name = "catalog"
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("contacts/", contacts, name="contacts"),
-    path("product_details/<int:pk>/", product_details, name="product_details"),
-]
+    path("", CatalogListView.as_view(), name="product_list"),
+    # path("", home, name="home"),
+    # path("contacts/", contacts, name="contacts"),
+    path("contacts/", CatalogTemplateView.as_view(), name="contacts"),
+    # path("product_details/<int:pk>/", product_details, name="product_details"),
+    path("product_detail/<int:pk>/", CatalogDetailView.as_view(), name="product_detail"),
 
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
